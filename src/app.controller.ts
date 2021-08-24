28d545f1f69d46ae937d9e83d29b6d59befd356e
seunghwako <seunghwa333@gmail.com>
2021-08-24 18:15:31 +0900

    Feat: app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello')
+  sayHello(): string {
+    return 'Hello Everyone!';
+  }
+
+  @Post('/hi')
+  sayHi(): string {
+    return 'Hello Everyone!';
   }
 }
