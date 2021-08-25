cd1c86973fedf2b1e3e38500e101c731803d6d16
seunghwako <seunghwa333@gmail.com>
2021-08-25 16:13:07 +0900

    Feat: Make DTO and ValidationPipes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,6 +10,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { urlToHttpOptions } from 'url';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -22,23 +23,24 @@ export class MoviesController {
     return this.moviesService.getAll();
   }
 
-  @Get('/:id')
-  getOne(@Param('id') id: string): Movie {
+  @Get(':id')
+  getOne(@Param('id') id: number): Movie {
+    console.log(typeof id);
     return this.moviesService.getOne(id);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.moviesService.deleteOne(id);
   }
 
   @Patch('/:id')
-  patch(@Param('id') id: string, @Body() updateData) {
+  patch(@Param('id') id: number, @Body() updateData) {
     return this.moviesService.update(id, updateData);
   }
 }
